d7ea4578926fa1f571bd5b3894bc4ae41aa48a8c
Thiago <thiagofreitascarneiro@hotmail.com>
2023-09-20 17:32:45 -0300

    feat: criando  sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,43 @@
+import { BarChart, CheckSquare, Cog, Flag, Home, LifeBuoy, Search, SquareStack, Users } from "lucide-react";
 import { Logo } from "./logo";
+import { NavItem } from "./NavItem";
+import { UsedSpaceWidget } from "./UsedSpaceWidget";
+import { Profile } from "./Profile";
 
 export function Sidebar() {
-    return  <aside className='border-r border-zinc-200 px-5 py-8'>
-        <Logo />
-    </aside>
+    return  (
+        <aside className='flex flex-col gap-6 border-r border-zinc-200 px-5 py-8'>
+            <Logo />
+
+            <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+                <Search className="h-5 w-5 text-zinc-500" />
+                <input 
+                    className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-900" 
+                    placeholder="Search"
+                />
+            </div>
+
+            <nav className="space-y-0.5">
+                <NavItem title="Home" icon={Home} />
+                <NavItem title="Dashboard" icon={BarChart} />
+                <NavItem title="Projects" icon={SquareStack} />
+                <NavItem title="Tasks" icon={CheckSquare} />
+                <NavItem title="Reporting" icon={Flag} />
+                <NavItem title="Users" icon={Users} />
+            </nav>
+
+            <div className="mt-auto flex flex-col gap-6">
+                <nav className="space-y-0.5"> 
+                    <NavItem title="Support" icon={LifeBuoy} />
+                    <NavItem title="Settings" icon={Cog} />
+                </nav>
+
+                <UsedSpaceWidget />
+
+                <div className="h-px bg-zinc-200" />
+
+                <Profile />
+            </div>
+        </aside>
+    )
 }
--- src/components/Sidebar/logo.tsx
+++ src/components/Sidebar/logo.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 export function Logo() {
   return (
-    <strong className="flex items-center gap-2 texto-xl font-semibold text-zinc-900">
+    <strong className="flex mx-1 items-center gap-2 texto-xl font-semibold text-zinc-900">
         <svg
       width={38}
       height={38}
