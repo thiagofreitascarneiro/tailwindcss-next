c56d81b4ca1d02d0027632429cb8e1afa9903e43
antena1thiago <thiago.carneiro@antena1.com.br>
2023-10-06 08:54:40 -0300

    feat: resposive page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <SettingsTabs />
 
       <div className="mt-6 flex flex-col ">
-        <div className="flex items-center justify-between border-b border-zinc-200 pb-5">
+      <div className="flex flex-col justify-between gap-4 border-b border-zinc-200 pb-5 lg:flex-row lg:items-center">
           <div className="space-y-1">
             <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
             <span className="text-sm text-zinc-900">
